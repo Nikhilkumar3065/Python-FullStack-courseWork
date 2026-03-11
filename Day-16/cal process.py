20802cefa97e826d742b2f18f7471360a26883bd
Nikhilkumar3065 <obulshettynikhilkumar@gmail.com>
2026-03-11 19:53:25 +0530

    Add files via upload

---
+++ Day-16/cal process.py
@@ -0,0 +1,18 @@
+'''
+from calculator.add import add
+from calculator.sub import sub
+from calculator.mul import mul
+from calculator.div import div
+print(add(4,5))
+print(sub(4,2))
+print(mul(4,4))
+print(div(4,10))
+
+#init
+from calculator import add,sub,mul,div
+print(add(4,5))
+print(sub(4,2))
+print(mul(4,4))
+print(div(4,10))
+'''
+
